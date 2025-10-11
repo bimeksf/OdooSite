f097c8201b163a4efcb69380ddbd5e74dad179d1
bimeksf <d3disek@gmail.com>
2025-10-11 10:40:17 +0200

    animace

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,12 +1,36 @@
+"use client"
 import React from 'react'
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 import { Avatar, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { ArrowUpRight } from "lucide-react";
-
+import { motion } from 'motion/react';
 const CardComponent = () => {
+  const MotionCard = motion(Card);
   return (
-    <Card className="w-[25.875rem] h-[31.25rem] flex items-start   p-[1.5rem] text-left border-4 border-black rounded-none">
+    <MotionCard
+      initial={{
+        borderRadius: 0,
+        backgroundColor: "#ffffff",
+        borderColor: "#000000",
+        color: "#000000",
+      }}
+      whileHover={{
+        background: "#ebe9da",
+        color: "#3e4f42",
+        borderTopRightRadius: 100,
+        borderColor: "transparent",
+      }}
+      transition={{
+        type: "tween",
+        duration: 0.25, // celková doba
+        ease: "easeOut",
+        borderRadius: { duration: 0.3 },
+        backgroundColor: { duration: 0.25 },
+        borderColor: { duration: 0.25 },
+      }}
+      className="w-[25.875rem] h-[31.25rem] flex items-start   p-[1.5rem] text-left border-4 border-black rounded-none"
+    >
       <CardHeader className="p-0 flex-shrink-0 pb-[1rem]">
         <Avatar className="h-[5rem] w-[5rem]">
           <AvatarImage src="/crm.svg" alt="Odoo ERP" />
@@ -26,7 +50,7 @@ const CardComponent = () => {
           <ArrowUpRight style={{ width: "50px", height: "50px" }} />
         </Button>
       </CardFooter>
-    </Card>
+    </MotionCard>
   );
 }
 
